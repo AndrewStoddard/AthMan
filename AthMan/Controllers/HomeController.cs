28facb35d7b42acae916c7f45e99a1177426249f
AndrewStoddard <acs8182000@gmail.com>
2021-03-08 12:10:47 -0500

    Added action for Items Page

--- AthMan/Controllers/HomeController.cs
+++ AthMan/Controllers/HomeController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AthMan.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AthMan.Controllers
 {
     public class HomeController : Controller
     {
+        private AthManContext context { get; set; }
+        public HomeController(AthManContext ctx)
+        {
+            this.context = ctx;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult Items()
+        {
+            ViewBag.Items = context.Items;
             return View();
         }
     }
