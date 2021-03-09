015995bd92237f687feade4ca71691185982f754
AndrewStoddard <acs8182000@gmail.com>
2021-03-09 18:49:23 -0500

    Added Add, Edit, Delete, View to Employees Controller and Views

--- AthMan/Controllers/EmployeesController.cs
+++ AthMan/Controllers/EmployeesController.cs
@@ -16,7 +16,66 @@ namespace AthMan.Controllers
             ViewBag.Employees = context.Employees;
             return View();
         }
-   
+
+        [HttpGet]
+        public IActionResult View(int employeeId)
+        {
+            Employee employee = this.context.Employees.Find(employeeId);
+            ViewBag.Action = "View";
+            return View("AddEditEmployee", employee);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            Employee employee = new Employee();
+            ViewBag.Action = "Add";
+            return View("AddEditEmployee", employee);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int employeeId)
+        {
+            Employee employee = this.context.Employees.Find(employeeId);
+            ViewBag.Action = "Edit";
+            return View("AddEditEmployee", employee);
+        }
+        [HttpGet]
+        public IActionResult Delete(int employeeId)
+        {
+            Employee employee = this.context.Employees.Find(employeeId);
+            return View("DeleteEmployee", employee);
+        }
+
+        [HttpPost]
+        public IActionResult AddEdit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                if (employee.EmployeeID == 0)
+                {
+                    this.context.Employees.Add(employee);
+                }
+                else
+                {
+                    this.context.Employees.Update(employee);
+                }
+                this.context.SaveChanges();
+                return RedirectToAction("Employees");
+            }
+            else
+            {
+                return View("AddEdit", employee);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Employee employee)
+        {
+            this.context.Employees.Remove(employee);
+            this.context.SaveChanges();
+            return RedirectToAction("Employees");
+        }
+
 
     }
 }
