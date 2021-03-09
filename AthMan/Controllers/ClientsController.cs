51033f01b0b6423c69f6f430302234a79ceb94d0
AndrewStoddard <acs8182000@gmail.com>
2021-03-09 18:02:40 -0500

    Added functionality to client controller

--- AthMan/Controllers/ClientsController.cs
+++ AthMan/Controllers/ClientsController.cs
@@ -17,7 +17,66 @@ namespace AthMan.Controllers
             ViewBag.Clients = context.Clients;
             return View();
         }
-       
+
+        [HttpGet]
+        public IActionResult View(int clientId)
+        {
+            Client client = this.context.Clients.Find(clientId);
+            ViewBag.Action = "View";
+            return View("AddEditClient", client);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            Client client = new Client();
+            ViewBag.Action = "Add";
+            return View("AddEditClient", client);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int clientId)
+        {
+            Client client = this.context.Clients.Find(clientId);
+            ViewBag.Action = "Edit";
+            return View("AddEditClient", client);
+        }
+        [HttpGet]
+        public IActionResult Delete(int clientId)
+        {
+            Client client = this.context.Clients.Find(clientId);
+            return View("DeleteClient", client);
+        }
+
+        [HttpPost]
+        public IActionResult AddEdit(Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                if (client.ClientID == 0)
+                {
+                    this.context.Clients.Add(client);
+                }
+                else
+                {
+                    this.context.Clients.Update(client);
+                }
+                this.context.SaveChanges();
+                return RedirectToAction("Clients");
+            }
+            else
+            {
+                return View("AddEdit", client);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Client client)
+        {
+            this.context.Clients.Remove(client);
+            this.context.SaveChanges();
+            return RedirectToAction("Clients");
+        }
+
 
     }
 }
